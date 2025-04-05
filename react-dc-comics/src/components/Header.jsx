921e746170acda096331de582edd97b570a07690
alemalagni <alemalagni@gmail.com>
2025-04-05 18:41:05 +0200

    feat(header.jsx): add NavCard

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import NavCard from "./NavCard.jsx";
+
 export default function Header() {
     return (
         <header>
-            <img src="../../img/dc-logo.png" />
+            <><img src="../../img/dc-logo.png" /></>
+            <><NavCard></NavCard></>
         </header>
     );
 }
