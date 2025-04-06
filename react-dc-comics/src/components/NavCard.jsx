83723b677801dc86441587d79674481ee05276d3
alemalagni <alemalagni@gmail.com>
2025-04-06 16:41:27 +0200

    feat(NavCard.jsx): add script

--- react-dc-comics/src/components/NavCard.jsx
+++ react-dc-comics/src/components/NavCard.jsx
@@ -1,5 +1,10 @@
-export default function NavCard() {
-    return (
-        <div>lista</div>
-    );
-}
+import React, { useState } from 'react';
+import NavItem from './NavItem';
+const navItems = ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP'];
+
+const NavCard = () => {
+    const [active, setActive] = useState('COMICS');
+
+};
+
+export default NavCard;
