051440fcd5718be099ff71407bcde59ddeff558c
alemalagni <alemalagni@gmail.com>
2025-04-07 10:07:11 +0200

    feat(Footer.jsx): add FooterBlock, AboutUs, SignUp

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,8 +1,13 @@
+import FooterBlock from "./FooterBlock";
 import AboutUs from "./AboutUs";
+import SignUp from "./SignUp";
 
 export default function Footer() {
     return (
         <footer>
+            <FooterBlock></FooterBlock>
             <AboutUs></AboutUs>
-        </footer>);
+            <SignUp></SignUp>
+        </footer>
+    );
 }
