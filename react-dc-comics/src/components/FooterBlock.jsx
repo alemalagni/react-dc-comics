cb089bbada58da5893eef378008c59ecd590ba62
alemalagni <alemalagni@gmail.com>
2025-04-07 10:20:57 +0200

    feat(FooterBlock.jsx): add FooterBlockCard

--- react-dc-comics/src/components/FooterBlock.jsx
+++ react-dc-comics/src/components/FooterBlock.jsx
@@ -1,3 +1,6 @@
+import FooterBlockCard from './FooterBlockCard.jsx';
+const CardItems = ['DIGITAL COMICS', 'DC MERCHANDISE', 'SUBSCRIPTION', 'COMIC SHOP LOCATOR', 'DC POWER VISA'];
+
 export default function FooterBlock() {
     return (
         <div>
