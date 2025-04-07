66a89e7b14c2ff668b4f4df6509747ac28b1fe7f
alemalagni <alemalagni@gmail.com>
2025-04-07 11:21:10 +0200

    feat(AboutUs.jsx): add Shop list

--- react-dc-comics/src/components/AboutUs.jsx
+++ react-dc-comics/src/components/AboutUs.jsx
@@ -1,16 +1,29 @@
 import AboutUsList from "./AboutUsList";
-const CardItems0 = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+const CardItems0 = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News'];
+const CardItems1 = ['Shop DC', 'Shop DC Collectibles'];
 
 const AboutUs = () => {
     return (
-        <div>
-            <strong>DC COMICS</strong>
-            {CardItems0.map((item) => (
-                <AboutUsList
-                    key={item}
-                    label={item}
-                />
-            ))};
+        <div style={{ display: 'flex' }}>
+            <div>
+                <strong>DC COMICS</strong>
+                {CardItems0.map((item) => (
+                    <AboutUsList
+                        key={item}
+                        label={item}
+                    />
+                ))}
+                <strong>SHOP</strong>
+                {CardItems1.map((item) => (
+                    <AboutUsList
+                        key={item}
+                        label={item}
+                    />
+                ))}
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
