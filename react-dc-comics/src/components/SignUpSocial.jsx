221919e54d51b8f52f6a31965580b9a63076e622
alemalagni <alemalagni@gmail.com>
2025-04-07 11:44:34 +0200

    test(SignUpSocial.jsx):

---
+++ react-dc-comics/src/components/SignUpSocial.jsx
@@ -0,0 +1,11 @@
+const SignUpSocial = () => {
+    return (
+        <div>
+            <a>
+                <img
+                    src="./img/footer-facebook"
+                />
+            </a>
+        </div>
+    );
+};
