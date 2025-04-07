96d66f1d3572df49276c908362e20a2e562a0f36
alemalagni <alemalagni@gmail.com>
2025-04-07 11:27:39 +0200

    feat(AboutUs.jsx): add CardItems2 and CardItems3

--- react-dc-comics/src/components/AboutUs.jsx
+++ react-dc-comics/src/components/AboutUs.jsx
@@ -1,6 +1,8 @@
 import AboutUsList from "./AboutUsList";
 const CardItems0 = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News'];
 const CardItems1 = ['Shop DC', 'Shop DC Collectibles'];
+const CardItems2 = ['Terms Of Use', 'Privacy policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscriptions', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us'];
+const CardItems3 = ['DC', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
 
 const AboutUs = () => {
     return (
