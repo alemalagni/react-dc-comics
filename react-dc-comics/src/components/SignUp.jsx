183ae74c3647608705bdd7369d1a4394eb8653ee
alemalagni <alemalagni@gmail.com>
2025-04-07 13:36:45 +0200

    feat(SignUp.jsx): add social

--- react-dc-comics/src/components/SignUp.jsx
+++ react-dc-comics/src/components/SignUp.jsx
@@ -1,11 +1,16 @@
 import SignUpSocial from "./SignUpSocial";
-const Social = ['facebook', 'twitter', 'youtube', 'pinterest', 'periscope']
+const social = ['facebook', 'twitter', 'youtube', 'pinterest', 'periscope']
 
 const SignUp = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <strong> SIGN UP NOW! </strong>
-            <SignUpSocial />
+            {social.map((item) => {
+                <SignUpSocial
+                    key={social}
+                    social={social}
+                />
+            })}
         </div>
     );
 }
