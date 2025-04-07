089f215a1ebbbf727c8c7e54421caff991647c35
alemalagni <alemalagni@gmail.com>
2025-04-07 16:06:24 +0200

    feat(FooterBlockCard.jsx):

--- react-dc-comics/src/components/FooterBlockCard.jsx
+++ react-dc-comics/src/components/FooterBlockCard.jsx
@@ -3,7 +3,7 @@ import style from "./FooterBlockCard.module.css"
 const FooterBlockCard = ({ label, image }) => {
     return (
         <div className={style.style}>
-            <img src={`./img/buy-comics-subscriptions.png`} alt="" className={style.img} />
+            <img src={`./img/buy-comics-${image}.png`} alt="" className={style.img} />
             <div>{label}</div>
         </div>
     );
