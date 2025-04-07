0ba34da407d3b91afbb76280699577ef0e819c27
alemalagni <alemalagni@gmail.com>
2025-04-07 16:17:21 +0200

    style(footer.jsx): add background

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -2,9 +2,11 @@ import FooterBlock from "./FooterBlock";
 import AboutUs from "./AboutUs";
 import SignUp from "./SignUp";
 
+import style from "./Footer.module.css"
+
 export default function Footer() {
     return (
-        <footer>
+        <footer className={style.style}>
             <FooterBlock></FooterBlock>
             <AboutUs></AboutUs>
             <SignUp></SignUp>
