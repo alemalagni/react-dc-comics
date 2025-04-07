cfdaf85fbccd179d86e9d1cb27075c2b10d03640
alemalagni <alemalagni@gmail.com>
2025-04-07 15:55:24 +0200

    feat(FooterBlock.jsx): add CardImage

--- react-dc-comics/src/components/FooterBlock.jsx
+++ react-dc-comics/src/components/FooterBlock.jsx
@@ -2,6 +2,7 @@ import style from "./FooterBlock.module.css"
 
 import FooterBlockCard from './FooterBlockCard.jsx';
 const CardItems = ['DIGITAL COMICS', 'DC MERCHANDISE', 'SUBSCRIPTION', 'COMIC SHOP LOCATOR', 'DC POWER VISA'];
+const CardImage = ['digital-comics.png', 'merchandise.png', 'subscription.png', 'shop-locator.png', 'power-visa.svg'];
 
 const FooterBlock = () => {
     return (
