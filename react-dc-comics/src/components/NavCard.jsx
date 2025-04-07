d4d3e8fc25114aae231bc77dc1dfac61c5a14583
alemalagni <alemalagni@gmail.com>
2025-04-07 10:12:30 +0200

    refactor(NavCard.jsx): remove import react

--- react-dc-comics/src/components/NavCard.jsx
+++ react-dc-comics/src/components/NavCard.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import NavItem from './NavItem';
 const navItems = ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP'];
 
--- react-dc-comics/src/components/NavItem.jsx
+++ react-dc-comics/src/components/NavItem.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import style from "./NavItem.module.css";
 
 const NavItem = ({ label, onClick }) => {
