b705af341f38b1f31531aec6cac30c2b3c48c9ee
alemalagni <alemalagni@gmail.com>
2025-04-07 15:33:44 +0200

    fix: header logo

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -5,7 +5,7 @@ import NavCard from "./NavCard.jsx";
 export default function Header() {
     return (
         <header className={style.style}>
-            <><img src="../../img/dc-logo.png" /></>
+            <><img src="../../img/dc-logo.png" className={style.logo} /></>
             <><NavCard></NavCard></>
         </header>
     );
