b4bef8434f803e5fa802dd3393c854d7cb431f21
alemalagni <alemalagni@gmail.com>
2025-04-07 10:59:18 +0200

    feat(AboutUs.jsx): add CardItems0

--- react-dc-comics/src/components/AboutUs.jsx
+++ react-dc-comics/src/components/AboutUs.jsx
@@ -1,7 +1,18 @@
-export default function AboutUs() {
+import AboutUsList from "./AboutUsList";
+const CardItems0 = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+
+const AboutUs = () => {
     return (
         <div>
-            {'--> Content goes here <--'}
+            <strong>DC COMICS</strong>
+            {CardItems0.map((item) => (
+                <AboutUsList
+                    key={item}
+                    label={item}
+                />
+            ))};
         </div>
     );
-}
+};
+
+export default AboutUs
