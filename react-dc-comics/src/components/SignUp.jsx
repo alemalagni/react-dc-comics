6d5eaf8582b07c310b8d3ede04370068a8a44dce
alemalagni <alemalagni@gmail.com>
2025-04-07 13:32:13 +0200

    feat(SignUp.jsx): add Social array

--- react-dc-comics/src/components/SignUp.jsx
+++ react-dc-comics/src/components/SignUp.jsx
@@ -1,10 +1,11 @@
 import SignUpSocial from "./SignUpSocial";
+const Social = ['facebook', 'twitter', 'youtube', 'pinterest', 'periscope']
 
 const SignUp = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <strong> SIGN UP NOW! </strong>
-            <SignUpSocial></SignUpSocial>
+            <SignUpSocial />
         </div>
     );
 }
