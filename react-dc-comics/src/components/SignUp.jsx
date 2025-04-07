ee76227a7914fc70df3b84fc956f48cf3dfe18c9
alemalagni <alemalagni@gmail.com>
2025-04-07 11:51:39 +0200

    feat(SignUp.jsx): add social

--- react-dc-comics/src/components/SignUp.jsx
+++ react-dc-comics/src/components/SignUp.jsx
@@ -1,8 +1,10 @@
+import SignUpSocial from "./SignUpSocial";
+
 const SignUp = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <strong> SIGN UP NOW! </strong>
-
+            <SignUpSocial></SignUpSocial>
         </div>
     );
 }
