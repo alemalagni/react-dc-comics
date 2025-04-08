7a18b009967701e7979a6b38ca8594df0662afbd
alemalagni <alemalagni@gmail.com>
2025-04-08 12:23:35 +0200

    feat(Comic): add script

---
+++ react-dc-comics/src/components/Comic.jsx
@@ -0,0 +1,10 @@
+import style from "./Comic.module.css";
+import comic from "../comics.js";
+
+export default function Comic() {
+    return (
+        <div className={style.style}>
+
+        </div>
+    );
+}
