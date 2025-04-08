f9ea5e39991fa8e084cadebb7139435b1c5621fe
alemalagni <alemalagni@gmail.com>
2025-04-08 13:00:38 +0200

    fix(Comic):

--- react-dc-comics/src/components/Comic.jsx
+++ react-dc-comics/src/components/Comic.jsx
@@ -1,10 +1,10 @@
 import style from "./Comic.module.css";
 
-export default function Comic(image, title) {
+export default function Comic({ image, title }) {
     return (
         <div className={style.style}>
             <div><img src={image} alt="" /></div>
-            <div>{title}</div>
+            <div><p>{title}</p></div>
         </div>
     );
 }
