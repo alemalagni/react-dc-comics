1fea82b96c524b0b4672863ddbc817674a4cd1cc
alemalagni <alemalagni@gmail.com>
2025-04-08 11:31:28 +0200

    feat(Header): add <div>

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -4,9 +4,11 @@ import NavCard from "./NavCard.jsx";
 
 export default function Header() {
     return (
-        <header className={style.style}>
-            <><img src="../../img/dc-logo.png" className={style.logo} /></>
-            <><NavCard></NavCard></>
+        <header>
+            <div className={style.style}>
+                <><img src="../../img/dc-logo.png" className={style.logo} /></>
+                <><NavCard></NavCard></>
+            </div>
         </header>
     );
 }
