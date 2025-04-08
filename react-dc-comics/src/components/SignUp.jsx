2b475e64a4cb7fa37911fad23728aa1b6f614958
alemalagni <alemalagni@gmail.com>
2025-04-08 10:36:10 +0200

    feat(SignUp): add <div>

--- react-dc-comics/src/components/SignUp.jsx
+++ react-dc-comics/src/components/SignUp.jsx
@@ -5,15 +5,17 @@ const social = ['facebook', 'twitter', 'youtube', 'pinterest', 'periscope']
 
 const SignUp = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className={style.style}>
-            <strong> SIGN UP NOW! </strong>
-            <div style={{ display: 'flex', gap: '5px', alignItems: 'center' }}>
-                <strong> FOLLOW US </strong>
-                {social.map((item) => (
-                    <SignUpSocial
-                        social={item}
-                    />
-                ))}
+        <div className={style.style}>
+            <div>
+                <strong> SIGN UP NOW! </strong>
+                <div style={{ display: 'flex', gap: '5px', alignItems: 'center' }}>
+                    <strong> FOLLOW US </strong>
+                    {social.map((item) => (
+                        <SignUpSocial
+                            social={item}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
