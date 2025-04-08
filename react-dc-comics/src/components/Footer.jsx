1a1b82380445338d124b733526586cc0504b8b1e
alemalagni <alemalagni@gmail.com>
2025-04-08 11:46:31 +0200

    feat(Footer): add bg

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -7,6 +7,7 @@ import style from "./Footer.module.css"
 export default function Footer() {
     return (
         <footer className={style.style}>
+            <img src="./img/dc-logo-bg.png" alt="" className={style.bgImage} />
             <FooterBlock></FooterBlock>
             <AboutUs></AboutUs>
             <SignUp></SignUp>
