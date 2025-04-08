3fa87f0d1740f4c83baddf2385a02d026885f6b7
alemalagni <alemalagni@gmail.com>
2025-04-08 10:28:39 +0200

    feat(Main): add <div> and class

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -3,7 +3,9 @@ import style from "./Main.module.css";
 export default function Main() {
     return (
         <main className={style.main} >
-            <div> {'--> Content goes here <--'} </div>
+            <div>
+                <div className={style.text}> {'--> Content goes here <--'} </div>
+            </div>
         </main >
     );
 }
