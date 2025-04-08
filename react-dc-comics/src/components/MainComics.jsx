98059c59bac443252579b1b4647d222a50f6632c
alemalagni <alemalagni@gmail.com>
2025-04-08 12:32:04 +0200

    feat(MainComic): add script

---
+++ react-dc-comics/src/components/MainComics.jsx
@@ -0,0 +1,16 @@
+import style from "./MainComic.module.css";
+import comicArray from "../comics.js"
+import Comic from "./Comic.jsx"
+
+export default function MainComic(image, title) {
+    return (
+        <div className={style.style}>
+            {comicArray.map((item) => (
+                <Comic
+                    image={item.thumb}
+                    title={item.title}
+                />
+            ))}
+        </div>
+    );
+}
