c3c33935c5ee8276e8c955fef2067f9e6431994d
alemalagni <alemalagni@gmail.com>
2025-04-09 10:44:00 +0200

    feat(MainComics): add BlueBlock

--- react-dc-comics/src/components/MainComics.jsx
+++ react-dc-comics/src/components/MainComics.jsx
@@ -1,16 +1,22 @@
 import style from "./MainComics.module.css";
 import comicArray from "../comics.js";
 import Comic from "./Comic.jsx";
+import BlueBlock from "./BlueBlock.jsx";
 
 export default function MainComic(image, title) {
     return (
-        <div className={style.style}>
-            {comicArray.map((item) => (
-                <Comic
-                    image={item.thumb}
-                    title={item.series}
-                />
-            ))}
-        </div>
+        <>
+            <BlueBlock
+                label={'CURRENT SEIRES'}
+            />
+            <div className={style.style}>
+                {comicArray.map((item) => (
+                    <Comic
+                        image={item.thumb}
+                        title={item.series}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
