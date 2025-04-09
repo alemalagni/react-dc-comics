693a887ebf46be1594dbd86adedc4277b37deae9
alemalagni <alemalagni@gmail.com>
2025-04-09 12:03:20 +0200

    feat(FooterBlockCard): change <div> to <a>

--- react-dc-comics/src/components/FooterBlockCard.jsx
+++ react-dc-comics/src/components/FooterBlockCard.jsx
@@ -2,10 +2,10 @@ import style from "./FooterBlockCard.module.css"
 
 const FooterBlockCard = ({ label, image }) => {
     return (
-        <div className={style.style}>
+        <a href="#" className={style.style}>
             <img src={`./img/buy-comics-${image}`} alt="" className={style.img} />
             <div>{label}</div>
-        </div>
+        </a>
     );
 };
 
